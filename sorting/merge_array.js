6f11eb2c5b5ba75e97f2d1d4418423bd3222d3b4
alieeeeeeeen <elainechen77@icloud.com>
2020-09-08 14:17:05 +0800

    don't mutate the original array

--- sorting/merge_array.js
+++ sorting/merge_array.js
@@ -1,18 +1,23 @@
 function mergeArray(arr1, arr2) {
     let result = [];
-    while(arr1.length && arr2.length) {
-        if(arr1[0] > arr2[0]) {
-            result.push(arr2[0]);
-            arr2.shift();
-        } else if (arr1[0] < arr2[0]) {
-            result.push(arr1[0]);
-            arr1.shift();
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length) {
+        if(arr1[i] > arr2[j]) {
+            result.push(arr2[j]);
+            j++;
+        } else if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
         }
     }
-    if(arr1.length) {
-        result = [...result, ...arr1]
-    } else if (arr2.length) {
-        result = [...result, ...arr2]
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
     }
     return result;
 }
